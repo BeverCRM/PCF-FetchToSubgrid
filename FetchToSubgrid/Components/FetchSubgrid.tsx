fdbf47887aaa849059454e17d2b8ee4d5aa5d96f
Artur-Sahakyan <artursahakyan2019@mail.ru>
2022-11-18 18:08:21 +0400

    add <Fetch Subgrid's> styles and functionality

--- FetchToSubgrid/Components/FetchSubgrid.tsx
+++ FetchToSubgrid/Components/FetchSubgrid.tsx
@@ -1,35 +1,60 @@
 import * as React from 'react';
-import { DetailsList } from '@fluentui/react';
+import { DetailsList, DetailsListLayoutMode,
+  Spinner, SpinnerSize } from '@fluentui/react';
 import FetchService from '../Services/FetchService';
+import { useState, useEffect, useRef } from 'react';
 
 export interface IFetchShbgridProps {
   inputValue: string | null;
 }
 
 export const FetchSubgrid: React.FunctionComponent<IFetchShbgridProps> = props => {
+  const [ isLoading, setIsLoading ] = useState<boolean>(true);
+  const hasData = useRef<boolean>(false);
+  const entities = useRef([]);
 
-  const [ entities, setEntities ] = React.useState([]);
-  const [ hasData, setHasData ] = React.useState(false);
-
-  React.useEffect(() => {
+  useEffect(() => {
+    setIsLoading(true);
     FetchService.getFetchData().then(
       (records: any) => {
-        setHasData(true);
-        setEntities(records);
+        entities.current = records;
+        hasData.current = true;
+        setIsLoading(false);
       },
       (error: any) => {
+        entities.current = [];
+        hasData.current = false;
         console.error(error);
-        setHasData(false);
+        setIsLoading(false);
       });
   }, [props.inputValue]);
 
-  if (!hasData) return <span>Error</span>;
+  if (isLoading) {
+    return (
+      <div className='fetchSubgridControl'>
+        <div className='fetchLoading'>
+          <Spinner size={SpinnerSize.large} />
+          <p className='loadingText'>  Loading ...</p>
+        </div>
+      </div>);
+  }
+
+  if (!hasData.current) {
+    return <div className='fetchSubgridControl'>
+      <div className='infoMessage'>
+        <h1 className='infoMessageText'>No data available</h1>
+      </div>
+    </div>;
+  }
 
   return (
-    <DetailsList
-      items={entities}
-      styles={{ contentWrapper: { minHeight: 150 } }}
-    >
-    </DetailsList>
+    <div className='fetchSubgridControl'>
+      <DetailsList
+        items={entities.current}
+        layoutMode={DetailsListLayoutMode.fixedColumns}
+        styles={{ contentWrapper: { maxHeight: 200 } }}
+      >
+      </DetailsList>
+    </div>
   );
 };
