7e303822a607598b495cef92f07bcf3b6b49a1c3
Artur-Sahakyan <artursahakyan2019@mail.ru>
2023-03-10 12:11:21 +0400

    fixed issues with errorMessage

--- FetchToSubgrid/components/AppWrapper.tsx
+++ FetchToSubgrid/components/AppWrapper.tsx
@@ -6,7 +6,11 @@ import { Loader } from './Loader';
 
 export const AppWrapper: React.FC<IAppWrapperProps> = props => {
   const [isLoading, setIsLoading] = React.useState(false);
-  const [error, setError] = React.useState<any>(undefined);
+  const [error, setError] = React.useState<Error | undefined>(undefined);
+
+  React.useEffect(() => {
+    setError(props.error);
+  }, [props.error]);
 
   return <>
     { error && <InfoMessage error={error} dataverseService={props._service} /> }
